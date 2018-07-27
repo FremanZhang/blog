497615c5e5de4f041d9922e2e4c4397cef185ce0
Freman <freman@yeah.net>
2018-07-27 17:12:34 +0800

    python update

---
+++ ENV3.5/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/freman/Code/blog/ENV3.5/bin/python3.5
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ my_blog/article/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+
+class Article(models.Model):
+    title = models.CharField(max_length = 128)
+    category = models.CharField(max_length = 50, blank = True)
+    date = models.DateField(auto_now_add = True)
+    content = models.TextField(blank = True, null = True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-date']
