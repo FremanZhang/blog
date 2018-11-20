1c9f02df410a9a7f873c70ac8040a9c465e5b409
Freman <freman@yeah.net>
2018-11-20 13:03:53 +0800

    change IDE

--- my_blog/article/views.py
+++ my_blog/article/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from article.models import Article
 from datetime import datetime
@@ -11,7 +11,8 @@ def home(request):
 
 def detail(request, post_id):
     content_dict = {}
-    post = Article.objects.get(id = post_id)
+    # post = Article.objects.get(id = post_id)
+    post = get_object_or_404(Article, id=post_id)
     # output_str = ("title=%s, category=%s, date=%s, content=%s" % (post.title, post.category, post.date, post.content))
     # return HttpResponse(output_str)
     content_dict['post'] = post
